c0fa693466864ff3be08e87ebfeff136eae25c54
hiwa2719 <hiahmadyan@gmail.com>
2022-08-21 11:08:46 +0430

    adding accounts app and Profile model

---
+++ accounts/models.py
@@ -0,0 +1,17 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+
+class Profile(models.Model):
+    """Profile model for each User"""
+
+    USER_TYPES = [
+        ('g', 'Golden'),
+        ('s', 'Silver'),
+        ('t', 'Typical')
+    ]
+    type = models.CharField(max_length=1, choices=USER_TYPES, default='t',
+                            help_text='type of user Golden, Silver, Typical')
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
