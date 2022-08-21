975e748d2f88343ca33adfe88edb039ce1e0153b
hiwa2719 <hiahmadyan@gmail.com>
2022-08-21 14:58:30 +0430

    adding email config to settings

--- request_service/settings.py
+++ request_service/settings.py
@@ -125,3 +125,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+import keys
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = getattr(keys, 'EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = getattr(keys, 'EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = getattr(keys, 'EMAIL_HOST_USER')
