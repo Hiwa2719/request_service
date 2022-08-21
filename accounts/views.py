e1fa980f696a50b8264ed705565e24ee9d47a93a
hiwa2719 <hiahmadyan@gmail.com>
2022-08-21 14:06:04 +0430

    adding login_view

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.forms import AuthenticationForm
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.contrib.auth import login
 
 from .serializers import ProfileSerializer, UserCreationSerializer
 
@@ -13,3 +15,19 @@ def register_view(request):
         serializer = ProfileSerializer(user.profile)
         return Response(serializer.data)
     return Response({'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+def login_view(request):
+    data = {
+        'username': request.data.get('email'),
+        'password': request.data.get('password')
+    }
+
+    form = AuthenticationForm(data=data)
+    if form.is_valid():
+        user = form.get_user()
+        login(request, user)
+        serializer = ProfileSerializer(user.profile)
+        return Response(serializer.data)
+    return Response({'errors': form.errors}, status=status.HTTP_400_BAD_REQUEST)
