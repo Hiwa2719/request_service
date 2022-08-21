43cf52432d8fac4967e0c665dee7eeaebe8cf2ca
hiwa2719 <hiahmadyan@gmail.com>
2022-08-21 21:42:12 +0430

    updating ProfileModelAdmin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import Profile
+from django.contrib import messages
+from django.utils.translation import ngettext
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileModelAdmin(admin.ModelAdmin):
+    list_display = '__str__', 'type'
+    list_editable = 'type',
+    search_fields = '__str__', 'type'
+
+    actions = ['make_golden', 'make_silver', 'make_typical']
+
+    @admin.action(description='make selected as Golden')
+    def make_golden(self, request, queryset):
+        updated = queryset.update(type='g')
+        self.send_message(request, updated)
+
+    @admin.action(description='make selected as Silver')
+    def make_silver(self, request, queryset):
+        updated = queryset.update(type='s')
+        self.send_message(request, updated)
+
+    @admin.action(description='make selected as Typical')
+    def make_typical(self, request, queryset):
+        updated = queryset.update(type='t')
+        self.send_message(request, updated)
+
+    def send_message(self, request, updated):
+        self.message_user(request, ngettext(
+            '%d story was successfully marked as published.',
+            '%d stories were successfully marked as published.',
+            updated,
+        ) % updated, messages.SUCCESS)
