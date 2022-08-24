de4610af38a5cc5eb5d27c7cf65c1d639655a603
hiwa2719 <hiahmadyan@gmail.com>
2022-08-24 18:12:50 +0430

    adding DeletePermission

--- bills/permisssions.py
+++ bills/permisssions.py
@@ -1,6 +1,12 @@
 from rest_framework.permissions import BasePermission
 
+from .models import BillStates
 
-class IsStaff(BasePermission):
-    def has_permission(self, request, view):
-        return request.user.is_staff
+
+class DeletePermission(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method == 'DELETE':
+            if obj.state in [BillStates.CHECK, BillStates.FINALIZED]:
+                self.message = 'Sorry your are not allowed to delete this request anymore.'
+                return False
+        return True
--- bills/views.py
+++ bills/views.py
@@ -1,18 +1,17 @@
 from django.contrib.auth import get_user_model
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
-from .models import BillStates
-from .permisssions import IsStaff
+from .permisssions import DeletePermission
 from .serializers import BillSerializer
 
 User = get_user_model()
 
 
 @api_view()
-@permission_classes([IsStaff])
+@permission_classes([IsAdminUser])
 def get_user_bills(request, user_id):
     try:
         user = User.objects.get(id=user_id)
@@ -25,7 +24,7 @@ def get_user_bills(request, user_id):
 
 
 class BillViewSet(ModelViewSet):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, DeletePermission]
     serializer_class = BillSerializer
 
     def get_queryset(self):
@@ -35,8 +34,3 @@ class BillViewSet(ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
-    def destroy(self, request, *args, **kwargs):
-        instance = self.get_object()
-        if instance.state in [BillStates.CHECK, BillStates.FINALIZED]:
-            return Response({'error': 'Your are not allowed to delete this bill anymore'})
-        return super().destroy(request, *args, **kwargs)
